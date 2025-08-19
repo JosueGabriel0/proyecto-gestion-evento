3078dd6fdc9b39e74511034b2fac8909eb151ab2
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2025-08-19 16:18:44 -0500

    creacion del proyecto

---
+++ backend/gestion-evento-laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuario registrado correctamente',
+            'user' => $user
+        ]);
+    }
+
+    public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Credenciales incorrectas'], 401);
+    }
+
+    // Crear token de acceso personal
+    $token = $user->createToken('Personal Access Token')->accessToken;
+
+    return response()->json([
+        'token' => $token,
+        'user' => $user
+    ]);
+}
+}
